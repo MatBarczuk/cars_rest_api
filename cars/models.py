8867d8b381f0003e2a92cf3467ed3cefa8d8b85b
Mateusz Barczuk <matbarczuk@gmail.com>
2021-06-09 22:14:16 +0200

    add models for car makes and car models

---
+++ cars/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class CarMake(models.Model):
+    make = models.CharField(max_length=50, null=False, unique=True)
+
+    def __str__(self):
+        return f'Make: {self.make}'
+
+
+class CarModel(models.Model):
+    make = models.ForeignKey('CarMake', related_name='car_model', on_delete=models.CASCADE)
+    model = models.CharField(max_length=100, null=False)
+
+    RATE_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5')
+    ]
+    rate = models.PositiveIntegerField(choices=RATE_CHOICES)
+    rates_number = models.PositiveIntegerField(default=0)
+    rates_sum = models.PositiveIntegerField(default=0)
+    avg_rating = models.DecimalField(max_digits=2, decimal_places=1)
+
+    def __str__(self):
+        return f'{self.make}, model: {self.model}'
